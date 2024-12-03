d1c002c0bd9ebfda356d5c95be9384b2c330a0e1
bunchofletters <realoscarhere@gmail.com>
2024-12-03 18:48:35 -0500

    Update some Information for GFL2 Combat and other minor changes

---
+++ ascripts.js
@@ -0,0 +1,48 @@
+// For collapsing menu item
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
+// End of collapsing menu item
+
+//Slideshow
+let slideindex = 1;
+showSlides(slideindex);
+
+function plusSlides(n){
+  showSlides(slideindex += n);
+}
+
+function currentslide(n){
+  showSlides(slideindex = n);
+}
+
+function showSlides(n){
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideindex = 1}
+  else if (n < 1) {slideindex = slides.length}
+  
+  for(i = 0; i< slides.length; i++){
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i<dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideindex-1].style.display = "block";
+  dots[slideindex-1].className += " active";
+}
